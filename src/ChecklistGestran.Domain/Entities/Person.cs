cf5e595141202e3c582f808a514aeb3cbc3520be
Guilherme Guimarães Uhrigsha <guilherme.guimaraes@tce.pr.gov.br>
2024-08-23 10:35:46 -0300

    slid

--- src/ChecklistGestran.Domain/Entities/Person.cs
+++ src/ChecklistGestran.Domain/Entities/Person.cs
@@ -5,9 +5,9 @@ namespace ChecklistGestran.Entities.Domain;
 public abstract class Person
 {
     public Guid Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
     public DateTime DateOfBirth { get; set; }
 
     public abstract bool AreDatesValid();
